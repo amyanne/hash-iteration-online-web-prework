d38626ac8252a3bfc30e8f6697415cba31b5bf58
Amy Westcott <amy.westcott@yahoo.com>
2019-06-06 21:20:46 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if :kids_name && age < 13 
-      happy_birthday(birthday_kids)
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     
   else 
     puts "You are too old for this."
