d49f0b69997ba9cb449729d475e1b5625d230cd4
Amy Westcott <amy.westcott@yahoo.com>
2019-06-06 21:12:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 13 
       happy_birthday(birthday_kids)
-    end
+    
   else 
     puts "You are too old for this."
   end
